cd863a91aa64198a5b4173d1159ea0ef2612f637
alexmasukov <alienspro2008@yandex.ru>
2019-11-15 21:31:49 +0900

    filterEntitie.js move to /constants. Delete files trash

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { filtersEntities } from 'utils/filtersEntities'
+import { filtersEntities } from 'constants/filtersEntities'
 import ButtonsGroupCollapseContainer from "containers/buttonsGroupCollapse"
 import styles from "components/Filter/Filter.module.sass"
 
@@ -8,10 +8,8 @@ const Filter = () => (
   <div className={styles.filter}>
     {
       // Более функциональный стиль, чем с Object.keys
-      // Потому что, мы будем не из нутри обращаться к this.props.filter[key]
-      Object.entries(filtersEntities)
-      // .sort((a, b) => a[1].order > b[1].order ? 1 : -1)
-        .map(item => {
+      // Потому что, мы не будем из нутри обращаться к filtersEntities[key]
+      Object.entries(filtersEntities).map(item => {
           const [filterKey, filter] = item
           return (
             <ButtonsGroupCollapseContainer
